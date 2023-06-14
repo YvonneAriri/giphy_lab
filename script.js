0cd489c6f92399f96e7b4ff997d920094ae520cd
Yvonne Ariri <yvonneariri@meta.com>
2023-06-14 11:18:20 -0700

    Added submit button event listener

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello")
+var submitBtn = document.getElementById("submitBtn");
+
+submitBtn.addEventListener("click", (event)=>{
+    event.preventDefault();
+    console.log("Button Clicked")
+})
